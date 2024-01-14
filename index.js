d6d155577a118fdf6ed81ba9dc732ebf47c14689
Dreller <dreller2034@gmail.com>
2024-01-14 12:00:59 -0500

    Dump
    File Dump.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electronIpcMain = require('electron').ipcMain;
 const path = require('node:path');
 const crypto = require('crypto');
 const jm = require('json-db-memory');
+const fs = require('fs');
 const { default: JsonDataStore } = require('json-db-memory');
 
 console.log( '>>> NEW TAX INSTANCE <<<' );
@@ -47,20 +48,14 @@ app.on('window-all-closed', () => {
  *   - Create New Database:
  *      Request:
  *          {
- *              method: 'new',
+ *              method: 'DatabaseCreate',
  *              year: 2024
- *          }
- *      Response:
- *          {
- *              file: 'guid'
  *          }
  * 
  */
 electronIpcMain.handle('engine', ( event, MyObject ) => {
     if( MyObject.constructor === Object ){
-        if( MyObject.method == "new" ){
-            DatabaseCreate( { year: MyObject.year } );
-        }
+        if( MyObject.method == "DatabaseCreate" ){ DatabaseCreate(MyObject) }
     }else{
         return false;
     }
@@ -73,8 +68,49 @@ electronIpcMain.handle('test', ( event, Message ) => {
 });
 
 function DatabaseCreate( ReqObject ){
-    var dbName = ReqObject.year + '.jtax';
-    var dbPath = process.env.INIT_CWD + '/data/' + dbName;
-    var myDB = new JsonDataStore( dbName );
-    myDB.set( 'year', ReqObject.year );
+    console.log( `Call to: DatabaseCreate() `);
+    console.log( ReqObject );
+    var dbName = ReqObject.year;
+    // Look for an existing file
+    var existingFiles = [];
+    fs.readdir( './node_modules/json-db-memory/data', function( err, files ){
+        if( err ){
+            console.log('Unable to scan directory: ' + err );
+        }
+        files.forEach(function(file){
+            existingFiles.push( file );
+        });
+        console.log( existingFiles );
+    });
+    if( existingFiles.includes( dbName + ".json" )){
+        console.error("DATABASE ALREADY EXISTS!");
+        return false;
+    }else{
+        var myDB = new JsonDataStore( dbName );
+        myDB.set( 'info', JSON.stringify({year: ReqObject.year}) );
+        myDB.set( 'suppliers', '' );
+        myDB.set( 'persons', '' );
+        myDB.set( 'domains', '' );
+
+        var DB = DatabaseLoad( { file: dbName } );
+        console.log( 'Result of DatabaseCreate: ');
+        console.log( DB );
+        return DB;
+    }
+
+}
+
+function DatabaseLoad( ReqObject ){
+    console.log( `Call to: DatabaseLoad()` );
+    console.log( ReqObject );
+    var myDB = new JsonDataStore( ReqObject.file );
+    var wip = {
+        info: JSON.parse( myDB.get("info") ),
+        suppliers: myDB.get("suppliers").split(";"),
+        persons: myDB.get("persons").split(";"),
+        domains: myDB.get("domains").split(";")
+    }
+    console.log( 'Result of DatabaseLoad: ');
+    console.log( wip );
+    return wip;
 }
